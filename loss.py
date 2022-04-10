3b8613e7f48b0728639f5ef319cc4f9d635a8b70
vanqaq <103369170+vanqaq@users.noreply.github.com>
2022-04-11 01:15:17 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,16 +1,20 @@
 Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 #loss
-import numpy as np
-
-def softmax(x):
-    max_x = np.max(x,axis=1).reshape(-1,1)
-    x = np.exp(x-max_x) / np.sum(np.exp(x-max_x), axis = 1, keepdims = True)
-    return x
-
-def CrossEntropyLoss(y_hat,y):
-    y_hat = softmax(y_hat)
-    l = 0
-    for i,j in enumerate(y):
-        l += -np.log(y_hat[i,j]+1e-05)
-    return l/len(y)
+class SoftmaxLossLayer(object):
+    def __init__(self):
+        pass
+    def forward(self,input):
+        input_max=np.max(input,axis=1,keepdims=True)
+        input_exp=np.exp(input-input_max)
+        self.prob=input_exp/np.sum(input_exp,axis=1,keepdims=True)
+        return self.prob
+    def get_loss(self,label):
+        self.batch_size=self.prob.shape[0]
+        self.label_onehot=np.zeros_like(self.prob)
+        self.label_onehot[np.arange(self.batch_size),label]=1.0
+        loss = -np.sum(np.log(self.prob)*self.label_onehot/self.batch_size)
+        return loss
+    def backward(self):
+        bottom_diff=(self.prob-self.label_onehot)/self.batch_size
+        return bottom_diff
