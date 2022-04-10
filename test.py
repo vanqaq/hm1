2041e94ee3225530e5fa552637a357358e6aa366
vanqaq <103369170+vanqaq@users.noreply.github.com>
2022-04-11 01:38:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from mlxtend.data import loadlocal_mnist
 from Mydataloader import MydataLoader
 from model import twolayers_mlp 
 
-X_test, y_test = loadlocal_mnist(
+x_test, y_test = loadlocal_mnist(
             images_path=r'C:\Users\13693\Desktop\mlnn\hm1\t10k-images-idx3-ubyte', 
             labels_path=r'C:\Users\13693\Desktop\mlnn\hm1\t10k-labels-idx1-ubyte')
             
@@ -16,19 +16,17 @@ def normalize(x):
 x_test = normalize(x_test)
 
 input_size = 28*28
-hidden_size = 256
+hidden_size = 128
 output_size = 10
-batch_size = 32
+batch_size = 64
 
 test_iter = MydataLoader(x_test,y_test,batch_size) 
 network = twolayers_mlp(input_size,hidden_size,output_size,lr = 0,l2 = 0)
-param = np.load(r'C:\Users\13693\Desktop\mlnn\hm1\param.npy', allow_pickle=True)
-network.load_state_dict(param)
+param = np.load(r'C:\Users\13693\Desktop\mlnn\hm1\mlp2-256hidden-0.1lr-0.0001l2.npy', allow_pickle=True)
+network.load_model(param)
 
 accuracy = 0
 for x,y in test_iter:
-    x = np.asarray(x)
-    y = np.asarray(y)
     y_hat = network(x)
     accuracy += (np.argmax(y_hat,axis=1)==y).sum()
 print('accuracy:',accuracy/len(test_iter))
